9d156ce4eb225f68df1cae5c0352c60790b35e43
Sulochan Khadka <122200551+Sulochan-khadka@users.noreply.github.com>
2024-08-04 17:25:05 +0530

    first commit

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,5 +1,5 @@
-import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
-import uniqid from "uniqid";
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import uniqid from 'uniqid';
 
 export async function POST(req) {
   const formData = await req.formData();
@@ -8,7 +8,7 @@ export async function POST(req) {
     const file = formData.get('file');
 
     const s3Client = new S3Client({
-      region: 'us-east-1',
+      region: 'eu-north-1',
       credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY,
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
@@ -25,17 +25,18 @@ export async function POST(req) {
       chunks.push(chunk);
     }
 
-    await s3Client.send(new PutObjectCommand({
-      Bucket: bucketName,
-      Key: newFilename,
-      ACL: 'public-read',
-      Body: Buffer.concat(chunks),
-      ContentType: file.type,
-    }));
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        ACL: 'public-read',
+        Body: Buffer.concat(chunks),
+        ContentType: file.type,
+      })
+    );
 
     const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
 
     return Response.json(link);
-
   }
 }
